bb5ec820057edc77cc8265401e6e9db3a2b64f7c
Felipe Bernardinello <felipebernardinello@gmail.com>
2026-01-19 12:32:33 -0300

    first commit

--- src/main/java/io/github/felipebernardinello/azurejay/AzurejayApplication.java
+++ src/main/java/io/github/felipebernardinello/azurejay/AzurejayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class AzurejayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(AzurejayApplication.class, args);
 	}
 
 }
